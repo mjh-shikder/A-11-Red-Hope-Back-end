48683fc30a7d6b4dcbef414e872d2b0923f198eb
mjh-shikder <mjhsikder@gmail.com>
2025-12-13 20:58:29 +0600

    basic index.js setup done

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors')
+require('dotenv').config({ path: '.env.local' });
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// Middleware
+app.use(cors());
+app.use(express.json())
+//
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.listen(port, () => {
+    console.log(`users server started on port: ${port}`);
+
+})
+
+
